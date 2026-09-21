692d8795e144cc41154dd474432f5125d658c317
NickButnar <103021759+NickButnar@users.noreply.github.com>
2026-09-21 16:12:22 +0400

    chore: add tanstack query

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,13 +1,17 @@
 import { Stack } from "expo-router";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "api/query-client";
 
 export default function RootLayout() {
   return (
-    <Stack>
+    <QueryClientProvider client={queryClient}>
+      <Stack>
       <Stack.Screen name="index" options={{ title: "Benchmarks" }} />
       <Stack.Screen name="flatlist" options={{ headerShown: false }} />
       <Stack.Screen name="flashlist" options={{ headerShown: false }} />
       <Stack.Screen name="final-form" options={{ headerShown: false }} />
       <Stack.Screen name="hook-form" options={{ headerShown: false }} />
     </Stack>
+    </QueryClientProvider>
   );
 }
